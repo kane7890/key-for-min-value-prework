31ee130b522f1475be59f7b6d17f5ca8adbdd876
Kane Koo <kanekoo@gmail.com>
2019-03-26 11:03:59 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,11 +1,11 @@
-require 'pry'
+#require 'pry'
  prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
   minkey=""
   minval-""
-  binding.pry
+ # binding.pry
   if name_hash =={}
     name_hash
   else
@@ -18,7 +18,7 @@ def key_for_min_value(name_hash)
       minval = value
       end
     end
-    binding.pry
+  #  binding.pry
   end
   minval
 end
